220064fcbd2f1e02b79dc8a910c239393cb824a1
Barni Seetharaman <barni@google.com>
2024-11-06 19:40:59 +0000

    fix script to correct inputs to moondream model and use correct output fields

--- models/moondream.py
+++ models/moondream.py
@@ -22,7 +22,7 @@ def main():
     model, example = benchmark.get_module()
 
     # Run the model once in torch
-    expected = model(*example)
+    expected = model(**example)
 
 
     env = torch_xla2.default_env()
@@ -36,16 +36,16 @@ def main():
     example = env.to_xla(example)
     with env:
         start = time.perf_counter()
-        xla2_ans = model(*example)
+        xla2_ans = model(**example)
         print(example)
         end = time.perf_counter()
         print('Eager mode time', end - start)
 
-    print('Eager max abs vs expected', (torch_xla2.tensor.j2t(xla2_ans._elem) - expected).abs().max())
+    print('Eager max abs vs expected: logits', (torch_xla2.tensor.j2t(xla2_ans.logits._elem) - expected.logits).abs().max())
 
     def func_call(state, example):
       with env:
-        return torch.func.functional_call(model, state, example, tie_weights=False)
+        return torch.func.functional_call(model, state, None, example, tie_weights=False)
 
     # doing it jitted
     jitted = torch_xla2.interop.jax_jit(func_call)
@@ -53,7 +53,7 @@ def main():
     xla2_ans = func_call(model.state_dict(), example)
     end = time.perf_counter()
     print('Jitted mode time', end - start)
-    print('Jitted max abs vs expected', (torch_xla2.tensor.j2t(xla2_ans._elem) - expected).abs().max())
+    print('Jitted max abs vs expected: logits', (torch_xla2.tensor.j2t(xla2_ans.logits._elem) - expected.logits).abs().max())
     return 0
 
 
